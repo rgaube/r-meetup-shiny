ee6b5727ea789efc5213a8795810f4938cbde593
Daniel Krieg <daniel.krieg@advisori.de>
2019-12-03 14:38:13 +0100

    - Added more content and exercises. - Improved on style and layout with rmdformats

--- solutions.R
+++ solutions.R
@@ -1,5 +1,15 @@
 
 
+library(shiny)
+library(dplyr)
+
+
+#### Constants ####
+
+page_style <- "background-color:rgb(0,170,112,0.3);"
+
+
+
 #### Example 1: Monte Carlo stock price evolution
 
 draw_price_path <- function(mean_return, volatility, 
@@ -49,10 +59,31 @@ ex2_ui <- fluidPage(style=page_style,
                     )
 )
 
+ex3_ui <- fluidPage(style=page_style,
+                    fluidRow(h2("Inputs"),
+                             column(4,sliderInput("mean_return", "Mean return", min=0, max=1, value=0.05)),
+                             column(4,sliderInput("volatility", "Volatility", min=0, max=0.5, value=0.1)),
+                             column(4,sliderInput("iterations", "Iterations", min=1, max=1000, value=100))
+                    ),
+                    fluidRow(h2("Outputs"),
+                             tabsetPanel(
+                               tabPanel("Paths", 
+                                        actionButton("replot", " ", icon("exclamation-triangle")),
+                                        plotOutput("paths")),
+                               tabPanel("Distribution", plotOutput("distribution")),
+                               tabPanel("Summary", tableOutput("distribution_summary")))
+                    )
+)
+
 
 ex1_server_fixed <- function(input, output, session) {
   
   library(ggplot2)
+  
+  plot_color <- rgb(153,27,86, 80, maxColorValue = 255)
+  
+  my_theme <- theme_classic() +
+    theme(plot.background = element_rect(fill = rgb(0,170,112,0.3*255, maxColorValue = 255)))
   
   mean_return <- 0.05
   volatility <- 0.1
@@ -62,9 +93,10 @@ ex1_server_fixed <- function(input, output, session) {
   
   output$paths <- renderPlot({
     price_paths %>% 
+      filter(day %% 5 == 1 | day == max(day))  %>%
       ggplot(aes(day, value, group=iteration)) +
-      geom_line(color=rgb(0,170,112,0.3*255, maxColorValue = 255)) +
-      theme_classic() +
+      geom_line(color=plot_color) +
+      my_theme +
       xlab("Days") +
       ylab("Stock price")
   })
@@ -72,8 +104,9 @@ ex1_server_fixed <- function(input, output, session) {
   output$distribution <- renderPlot({
     price_paths %>% filter(day == max(day)) %>% 
       ggplot(aes(value)) +
-      geom_histogram(fill=rgb(0,170,112,0.3*255, maxColorValue = 255)) + 
+      geom_histogram(fill=plot_color) + 
       theme_classic() +
+      theme(plot.background = element_rect(fill = rgb(0,170,112,0.3*255, maxColorValue = 255))) +
       xlab("Stock price") +
       ylab("Count")
   })
@@ -90,14 +123,19 @@ ex1_server_reactive <- function(input, output, session) {
   
   library(ggplot2)
   
+  plot_color <- rgb(153,27,86, 80, maxColorValue = 255)
+  
+  my_theme <- theme_classic() +
+    theme(plot.background = element_rect(fill = rgb(0,170,112,0.3*255, maxColorValue = 255)))
+  
   price_paths <- reactive({
     simulate_paths(input$iterations, input$mean_return, input$volatility)})
   
   output$paths <- renderPlot({
     price_paths() %>% 
       ggplot(aes(day, value, group=iteration)) +
-      geom_line(color=rgb(0,170,112,0.3*255, maxColorValue = 255)) +
-      theme_classic() +
+      geom_line(color=plot_color) +
+      my_theme +
       xlab("Days") +
       ylab("Stock price")
   })
@@ -106,8 +144,8 @@ ex1_server_reactive <- function(input, output, session) {
     price_paths() %>% 
       filter(day == max(day)) %>%
       ggplot(aes(value)) +
-      geom_histogram(fill=rgb(0,170,112,0.3*255, maxColorValue = 255)) + 
-      theme_classic() +
+      geom_histogram(fill=plot_color) + 
+      my_theme +
       xlab("Stock price") +
       ylab("Count")
   })
@@ -118,5 +156,54 @@ ex1_server_reactive <- function(input, output, session) {
       pull(value) %>%
       summary() %>% as.list()
     })
+}
+
+ex3_server_reactive <- function(input, output, session) {
+  
+  library(ggplot2)
+  
+  plot_color <- rgb(153,27,86, 80, maxColorValue = 255)
+  
+  my_theme <- theme_classic() +
+    theme(plot.background = element_rect(fill = rgb(0,170,112,0.3*255, maxColorValue = 255)))
+  
+  price_paths <- reactive({
+    simulate_paths(input$iterations, input$mean_return, input$volatility)})
+  
+  observeEvent(price_paths(), {
+    updateActionButton(session, "replot", label="Replot", icon=shiny::icon("exclamation-triangle"))
+  })
+  
+  observeEvent(input$replot, {updateActionButton(session, "replot", "Up-to-date", icon=shiny::icon("check-circle"))})
+  
+  delayed_price_paths_plot <- eventReactive(input$replot, {
+    price_paths() %>% 
+      ggplot(aes(day, value, group=iteration)) +
+      geom_line(color=plot_color) +
+      my_theme +
+      xlab("Days") +
+      ylab("Stock price")
+  })
+  
+  output$paths <- renderPlot({
+    delayed_price_paths_plot()
+    })
+  
+  output$distribution <- renderPlot({
+    price_paths() %>% 
+      filter(day == max(day)) %>%
+      ggplot(aes(value)) +
+      geom_histogram(fill=plot_color) + 
+      my_theme +
+      xlab("Stock price") +
+      ylab("Count")
+  })
+  
+  output$distribution_summary <- renderTable({
+    price_paths() %>% 
+      filter(day == max(day)) %>%
+      pull(value) %>%
+      summary() %>% as.list()
+  })
 }
 
