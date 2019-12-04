be26275d76f9070c0ac3f6866ac0c48bfb389bb1
Daniel Krieg <daniel.krieg@advisori.de>
2019-12-04 15:42:34 +0100

    add Modules example

--- solutions.R
+++ solutions.R
@@ -22,7 +22,7 @@ draw_price_path <- function(mean_return, volatility,
   tibble::tibble(day = seq(days_per_period),
                  period = day %% days_per_period,
                  log_return = rnorm(days_per_period*duration, 
-                                    mean=mean_return/days_per_period, 
+                                    mean=log(1+mean_return)/days_per_period, 
                                     sd=volatility/sqrt(days_per_period))) %>%
     mutate(return = exp(log_return),
            value = start*cumprod(return))
@@ -216,3 +216,94 @@ ex3_server_reactive <- function(input, output, session) {
   })
 }
 
+
+
+###########
+
+
+ex4_moduleInput <- function(id) {
+  ns <- NS(id)
+  
+  tagList(
+    fluidRow(h2("Inputs"),
+             column(4,sliderInput(ns("mean_return"), "Mean return", min=0, max=1, value=0.05)),
+             column(4,sliderInput(ns("volatility"), "Volatility", min=0, max=0.5, value=0.1)),
+             column(4,sliderInput(ns("iterations"), "Iterations", min=1, max=1000, value=100))
+    )
+  )
+}
+
+ex4_moduleOutput <- function(id) {
+  ns <- NS(id)
+  
+  tagList(
+    fluidRow(h2("Outputs"),
+             tabsetPanel(
+               tabPanel("Paths", 
+                        actionButton(ns("replot"), " ", icon("exclamation-triangle")),
+                        plotOutput(ns("paths"))),
+               tabPanel("Distribution", plotOutput(ns("distribution"))),
+               tabPanel("Summary", tableOutput(ns("distribution_summary"))))
+    )
+  )
+}
+
+
+ex4_calc_paths <- function(input, output, session) {
+  reactive({
+    simulate_paths(input$iterations, input$mean_return, input$volatility)})
+}
+
+ex4_plot <- function(input, output, session,
+                     price_paths,
+                     plot_color = rgb(153,27,86, 80, maxColorValue = 255),
+                     bg_color = rgb(0,170,112,0.3*255, maxColorValue = 255)) {
+  
+  library(ggplot2)
+  
+  ns <- session$ns
+
+  my_theme <- theme_classic() +
+    theme(plot.background = element_rect(fill = bg_color ))
+  
+  
+  
+  observeEvent(price_paths(), {updateActionButton(session, "replot", 
+                                                  label=button_labels$replot, 
+                                                  icon=shiny::icon("exclamation-triangle"))
+  })
+  
+  observeEvent(input$replot, {updateActionButton(session, "replot", 
+                                                 label=button_labels$ok,
+                                                 icon=shiny::icon("check-circle"))})
+  
+  delayed_price_paths_plot <- eventReactive(input$replot, {
+    price_paths() %>% 
+      ggplot(aes(day, value, group=iteration)) +
+      geom_line(color=plot_color) +
+      my_theme +
+      xlab("Days") +
+      ylab("Stock price")
+  })
+  
+  output$paths <- renderPlot({
+    delayed_price_paths_plot()
+  })
+  
+  output$distribution <- renderPlot({
+    price_paths() %>% 
+      filter(day == max(day)) %>%
+      ggplot(aes(value)) +
+      geom_histogram(fill=plot_color) + 
+      my_theme +
+      xlab("Stock price") +
+      ylab("Count")
+  })
+  
+  output$distribution_summary <- renderTable({
+    price_paths() %>% 
+      filter(day == max(day)) %>%
+      pull(value) %>%
+      summary() %>% as.list()
+  })
+}
